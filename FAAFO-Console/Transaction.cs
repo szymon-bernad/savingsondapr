0e2687e253d79453c3ed52576e139ad7afe0cb7c
Szymon Bernad <szymon.bernad@gmail.com>
2024-09-22 20:28:13 +0200

    added test console app

---
+++ FAAFO-Console/Transaction.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAAFOConsole;
+
+record Transaction(string ExternalRef, decimal Amount, DateTime TransactionDate, string OpType);

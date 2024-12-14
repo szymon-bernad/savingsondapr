2648b95498376b81da9560b189a44903b6d83207
Szymon Bernad <szymon.bernad@gmail.com>
2024-12-14 16:06:46 +0100

    faafo console removed

--- FAAFO-Console/Transaction.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FAAFOConsole;
-
-record Transaction(string ExternalRef, decimal Amount, DateTime TransactionDate, string OpType);

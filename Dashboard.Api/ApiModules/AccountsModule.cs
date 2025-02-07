c52a66280f4699fa57bc613a2284343f2df696b5
Szymon Bernad <szymon.bernad@gmail.com>
2025-02-07 10:54:25 +0100

    Dashboard API changes

--- Dashboard.Api/ApiModules/AccountsModule.cs
+++ Dashboard.Api/ApiModules/AccountsModule.cs
@@ -1,23 +1,23 @@
 ﻿using Carter;
 using Dashboard.Api.ApiClients;
-using SavingsPlatform.Contracts.Accounts.Models;
 using System.Security.Claims;
 
 namespace Dashboard.Api.ApiModules;
 
 public class AccountsModule : ICarterModule
 {
+    private const string OidClaimType = "http://schemas.microsoft.com/identity/claims/objectidentifier";
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapGet("/api/users/{userid}/accounts", 
             async (
-            string userid,
-            IAccountsApiClient apiClient,
-            ClaimsPrincipal user) =>
+                string userid,
+                IAccountsApiClient apiClient,
+                ClaimsPrincipal user) => 
         {
             if (user.Identity?.IsAuthenticated ?? false)
             {
-                var oidClaim = user.Claims?.FirstOrDefault(c => c.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier");
+                var oidClaim = user.Claims?.FirstOrDefault(c => c.Type == OidClaimType);
                 if (oidClaim?.Value == userid)
                 {
                     var accounts = await apiClient.GetAllUserAccountsAsync(userid);
--- SavingsPlatform.Accounts/AccountHolders/AccountHolder.cs
+++ SavingsPlatform.Accounts/AccountHolders/AccountHolder.cs
@@ -6,52 +6,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SavingsPlatform.Accounts.AccountHolders
+namespace SavingsPlatform.Accounts.AccountHolders;
+
+public class AccountHolder
+    (IStateEntryRepository<AccountHolderState> repository, 
+     AccountHolderState? state, ILogger<AccountHolder> logger) 
+    : IAggregateRoot<AccountHolderState>
 {
-    public class AccountHolder(IStateEntryRepository<AccountHolderState> repository, AccountHolderState? state, ILogger<AccountHolder> logger) 
-        : IAggregateRoot<AccountHolderState>
+    private AccountHolderState? _state = state;
+    private readonly IStateEntryRepository<AccountHolderState> _repository = repository;
+    private readonly ILogger<AccountHolder> _logger = logger;
+
+    public async Task Create(string key, string? username, string externalRef, string[] accountIds)
     {
-        private AccountHolderState? _state = state;
-        private readonly IStateEntryRepository<AccountHolderState> _repository = repository;
-        private readonly ILogger<AccountHolder> _logger = logger;
-
-        public async Task Create(string key, string? username, string externalRef, string[] accountIds)
+        var state = new AccountHolderState
         {
-            var state = new AccountHolderState
-            {
-                Key = key,
-                Username = username,
-                ExternalRef = externalRef,
-                AccountIds = accountIds,
-            };
+            Key = key,
+            Username = username,
+            ExternalRef = externalRef,
+            AccountIds = accountIds,
+        };
 
-            var exists = (await _repository.GetAccountAsync(externalRef)) is not null;
+        var exists = (await _repository.GetAccountAsync(externalRef)) is not null;
 
-            if (exists)
-            {
-                throw new InvalidOperationException($"AccountHolder already exists");
-            }
-
-            await _repository.AddAccountAsync(state);
+        if (exists)
+        {
+            throw new InvalidOperationException($"AccountHolder already exists");
         }
 
-        public Task AddAccount(string accountId)
-        {
-            if (_state is null)
-            {
-                throw new InvalidOperationException($"AccountHolder does not exist");
-            }
-
-            if(_state.AccountIds.Contains(accountId))
-            {
-                _logger.LogWarning($"Account {accountId} already assigned to AccountHolder {_state.Key}");
-                return Task.CompletedTask;
-            }
-
-            _state.AccountIds.Add(accountId);
-            return _repository.TryUpdateAccountAsync(_state, null);
-        }
-
-        public AccountHolderState? State => _state;
+        await _repository.AddAccountAsync(state);
     }
+
+    public Task AddAccount(string accountId)
+    {
+        if (_state is null)
+        {
+            throw new InvalidOperationException($"AccountHolder does not exist");
+        }
+
+        if(_state.AccountIds.Contains(accountId))
+        {
+            _logger.LogWarning($"Account {accountId} already assigned to AccountHolder {_state.Key}");
+            return Task.CompletedTask;
+        }
+
+        _state.AccountIds.Add(accountId);
+        return _repository.TryUpdateAccountAsync(_state, null);
+    }
+
+    public AccountHolderState? State => _state;
 }

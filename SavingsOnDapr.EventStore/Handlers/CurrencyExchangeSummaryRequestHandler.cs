5b40706b9a27e028e925cc7dffd4b728ac7170b3
Szymon Bernad <szymon.bernad@gmail.com>
2025-03-30 16:02:54 +0200

    better null value handling

--- SavingsOnDapr.EventStore/Handlers/CurrencyExchangeSummaryRequestHandler.cs
+++ SavingsOnDapr.EventStore/Handlers/CurrencyExchangeSummaryRequestHandler.cs
@@ -1,6 +1,7 @@
 ﻿using JasperFx.Core;
 using Microsoft.Extensions.Caching.Distributed;
 using SavingsOnDapr.EventStore.Store;
+using SavingsPlatform.Contracts.Accounts.Enums;
 using SavingsPlatform.Contracts.Accounts.Requests;
 using SavingsPlatform.Contracts.CurrencyExchange.Response;
 using System.Text.Json;
@@ -15,30 +16,46 @@ public class CurrencyExchangeSummaryRequestHandler(
     public async Task Handle(CurrencyExchangeSummaryRequest request)
     {
 
-        ICollection<string> streamIds = new List<string> { $"{request.SourceCurrency}=>{request.TargetCurrency}_{request.StartDate:yyyy-MM-dd}" };
+        var summaryTasks = new List<SummaryTaskData> { new SummaryTaskData(request.SourceCurrency, request.TargetCurrency, request.StartDate) };
 
         if (request.EndDate > request.StartDate)
         {
             var itDate = request.StartDate.AddDays(1);
             while (itDate <= request.EndDate)
             {
-                streamIds.Add($"{request.SourceCurrency}=>{request.TargetCurrency}_{itDate:yyyy-MM-dd}");
+                summaryTasks.Add(new SummaryTaskData(request.SourceCurrency, request.TargetCurrency, itDate));
                 itDate = itDate.AddDays(1);
             }
         }
 
-        var results = await Task.WhenAll(streamIds.Select(streamId => _store.GetSummary(streamId)));
-        var entries = results.Where(r => r is not null).Select(r => new CurrencyExchangeSummaryValueEntry 
-        {
-            EntryName = $"{request.SourceCurrency}=>{request.TargetCurrency}_{r.SummaryDate:yyyy-MM-dd}",
-            ColumnValues = 
-            [
-                    $"{r.SummaryDate}", 
-                    $"{r.TotalCountOfExchanges}", 
-                    $"{r.TotalSourceAmountOfExchanges}", 
-                    $"{r.TotalTargetAmountOfExchanges}" 
-            ], 
-        });
+        var results = await Task.WhenAll(
+            summaryTasks.Select(async st => {
+                var res = await _store.GetSummary($"{st.Source}=>{st.Target}_{st.Date:yyyy-MM-dd}");
+                res ??= new Aggregations.CurrencyExchangeSummaryDto
+                {
+                    SummaryDate = new DateOnly(st.Date.Year, st.Date.Month, st.Date.Day),
+                    SourceCurrency = st.Source,
+                    TargetCurrency = st.Target,
+                    TotalCountOfExchanges = 0,
+                    TotalSourceAmountOfExchanges = 0,
+                    TotalTargetAmountOfExchanges = 0,
+                    Entries = []
+                };
+                return res;
+            }));
+
+        var entries = results.Where(r => r is not null)
+            .Select(r => new CurrencyExchangeSummaryValueEntry 
+            {
+                EntryName = $"{request.SourceCurrency}=>{request.TargetCurrency}_{r.SummaryDate:yyyy-MM-dd}",
+                ColumnValues = 
+                [
+                        $"{r.SummaryDate}", 
+                        $"{r.TotalCountOfExchanges}", 
+                        $"{r.TotalSourceAmountOfExchanges}", 
+                        $"{r.TotalTargetAmountOfExchanges}" 
+                ], 
+            });
         var response = new CurrencyExchangeSummaryResponse
         {
             ResponseKey = $"{request.SourceCurrency}=>{request.TargetCurrency}_{request.StartDate:yyyy-MM-dd}_{request.EndDate:yyyy-MM-dd}",
@@ -51,4 +68,6 @@ public class CurrencyExchangeSummaryRequestHandler(
             AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(60)
         });
     }
+
+    internal record SummaryTaskData(Currency Source, Currency Target, DateOnly Date);
 }
